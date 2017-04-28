8f9c067a294b4946a5bdfd636a8d02d3af7c8bad
Jon Fuqua <jfuqua@locals-MacBook-Pro.local>
2017-04-28 16:50:55 -0400

    start 8

--- 1-10/7.py
+++ 1-10/7.py
@@ -1,5 +1,17 @@
 import math
 
+def sieve_of_eratosthenes(limit = 125):
+    sieve = [True] * limit
+    sieve[0] = sieve[1] = False
+
+    for (i, isprime) in enumerate(sieve):
+        if (isprime):
+            for n in range(i * i, limit, i):
+                sieve[n] = False
+    return sieve
+
+print(sieve_of_eratosthenes(100))
+
 def is_prime(n):
     for i in range(2, n):
         if n % i == 0:
@@ -18,4 +30,4 @@ def find_prime(pos):
         i+=1
     return result
 
-print(find_prime(10001))
+print(find_prime(100))
--- misc/primes.py
+++ misc/primes.py
@@ -9,4 +9,41 @@ def find_primes(n):
 
     return primes
 
-print(find_primes(17))
+print(find_primes(5000))
+
+def rwh_primes2(n):
+    """ Input n>=6, Returns a list of primes, 2 <= p < n """
+    correction = (n%6>1)
+    n = {0:n,1:n-1,2:n+4,3:n+3,4:n+2,5:n+1}[n%6]
+    sieve = [True] * (n/3)
+    sieve[0] = False
+    for i in xrange(int(n**0.5)/3+1):
+      if sieve[i]:
+        k=3*i+1|1
+        sieve[      ((k*k)/3)      ::2*k]=[False]*((n/6-(k*k)/6-1)/k+1)
+        sieve[(k*k+4*k-2*k*(i&1))/3::2*k]=[False]*((n/6-(k*k+4*k-2*k*(i&1))/6-1)/k+1)
+    return [2,3] + [3*i+1|1 for i in xrange(1,n/3-correction) if sieve[i]]
+
+def sieve2(n):
+    loops = 0
+    numbers = range(0, n)
+    for prime in numbers:
+        if prime < 2:
+            continue
+        elif prime > n ** 0.5:
+            break
+        for i in range(prime ** 2, n, prime):
+            numbers[i] = 0
+            loops += 1
+    return [x for x in numbers if x > 1], loops
+
+def sieve_of_eratosthenes(limit = 125):
+    sieve = [True] * limit
+    sieve[0] = sieve[1] = False
+
+    for (i, isprime) in enumerate(sieve):
+        if (isprime):
+            for n in range(i * i, limit, i):
+                sieve[n] = False
+    return sieve
+    
